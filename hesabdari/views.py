accc2dbec6d550e046b65cf8a44a11a9391cfcae
alireza <alirezamlc77@yahoo.com>
2021-07-27 22:15:27 +0430

    first time all files that created added

---
+++ hesabdari/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import IncomeSerializer
+from .models import Income
+# Create your views here.
+
+class IncomeViewSet(viewsets.ModelViewSet):
+    # define queryset
+    queryset = Income.objects.all()
+     
+    # specify serializer to be used
+    serializer_class = IncomeSerializer
